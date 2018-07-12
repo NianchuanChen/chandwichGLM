715adb5338a743cbc8fae4b808fd561949edd883
NianchuanChen <490447961@qq.com>
2018-07-12 11:51:18 +0100

    1

---
+++ R/chandwichGLM.R
@@ -0,0 +1,23 @@
+#' chandwichGLM: Chandler-Bate Sandwich Loglikelihood Adjustment for Generalized
+#' Linear Models
+#'
+#' Performs inferences from Generalized Linear Models fitted to cluster
+#' correlated data, using adjustment to the independence loglikelihood
+#' developed in
+#' \href{http://dx.doi.org/10.1093/biomet/asm015}{Chandler and Bate (2007)}.
+#' The adjustment are performed by the
+#' \code{\link[chandwich]{chandwich}}.
+#'
+#' @details
+#' Add details.
+#'
+#' See Chandler and Bate (2007) for full details and
+#' \code{vignette("chandwichGLM-vignette", package = "chandwich")} for an
+#' overview of the package.
+#' @references Chandler, R. E. and Bate, S. (2007). Inference for clustered
+#'   data using the independence loglikelihood. \emph{Biometrika},
+#'   \strong{94}(1), 167-183. \url{http://dx.doi.org/10.1093/biomet/asm015}
+#' @docType package
+#' @name chandwichGLM
+#' @import methods
+NULL
